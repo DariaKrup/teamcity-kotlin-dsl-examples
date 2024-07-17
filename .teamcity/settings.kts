75a00cb5bb93acaa9dcba87ada66260ce0d16f28
admin <admin@TeamCity>
2024-07-17 16:13:08 +0200

    TeamCity change in 'Teamcity Kotlin DSL Examples' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/settings.kts
@@ -0,0 +1,59 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.remoteParameters.hashiCorpVaultParameter
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2023.11"
+
+project {
+    description = "Project with TeamCity Kotlin DSL examples"
+
+    buildType(VaultParameterBuild)
+
+    template(BuildTemplate)
+}
+
+object VaultParameterBuild : BuildType({
+    templates(BuildTemplate)
+    name = "Build Configuration with Vault parameter"
+})
+
+object BuildTemplate : Template({
+    name = "build.template"
+
+    params {
+        hashiCorpVaultParameter {
+            name = "vault_parameter"
+            query = "/path/to/some/secret!value"
+        }
+    }
+
+    steps {
+        script {
+            name = "Parameter output"
+            id = "ParameterOutput"
+            scriptContent = "echo %vault_parameter%"
+        }
+    }
+})
